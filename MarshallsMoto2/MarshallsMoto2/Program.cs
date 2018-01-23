1c44a2ade0ec277d866d8a85d4ff71f5c3a665bf
mchalupa3 <mchalupa3@gmail.com>
2018-01-23 14:10:46 -0600

    update

---
+++ MarshallsMoto2/MarshallsMoto2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarshallsMoto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+            Console.WriteLine("M                            M");
+            Console.WriteLine("M Make your vision your view M");
+            Console.WriteLine("M                            M");
+            Console.WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+        }
+    }
+}
